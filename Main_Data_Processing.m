1bb5257f6cc20b9d4256f5c5a448e9db85088e71
Shelby Stowe <sstowe@mines.edu>
2026-09-03 13:49:14 -0600

    Create Main_Data_Processing.m

---
+++ Main_Data_Processing.m
@@ -0,0 +1,29 @@
+%% Main file to process Walch et al. (2019) data and data from Takeda 
+
+% Shelby Stowe
+% September 2026
+% Colorado School of Mines
+
+% Code outline: 
+    % Calls .m function to process the Walch data, stores relevant measurements 
+    % Calls .m function to process the Takeda data, stores relevant measurements
+    % Plots bar graphs of measurements from both data sets 
+
+%% Set up 
+
+clear all
+close all
+clc 
+
+%% Call .m function to process Walch et al. data 
+% 
+
+
+
+%% Call .m function to process the Takeda data
+% 
+
+
+%% Plot bar graphs of measurements from both data sets 
+
+
